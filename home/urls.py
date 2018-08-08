03031b745661fb8e6bed12b46833ee471d6ae749
sardar <sardarr@gmail.com>
2018-08-08 19:02:53 -0400

    My website

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name='home'
 urlpatterns = [
-    path('', views.Homeview.as_view() ,name='home')
-    ]
+    path('', views.Homeview.as_view() ,name='home'),
+]
 
--- home/views.py
+++ home/views.py
@@ -8,25 +8,25 @@ from django.conf import settings
 
 class Homeview(TemplateView):
     template_name = 'home/home.html'
+    pFlag=False
     def get(self,request):
-        model = settings.MODEL
-        tkz=settings.TOKENIZER
         form=HomeForm()
-        posts=Post.objects.all().order_by('-created')[:1]
-        # if Post
-        tagged=tag(model,tkz.tokenize(posts[0].post+"."))
-        args={'form':form,'posts':posts,'tagged':tagged}
-        return render(request,self.template_name,args)
+        args = {'form': form}
+        return render(request, self.template_name,args)
     def post(self,request):
         form =HomeForm(request.POST)
         if form.is_valid():
             post=form.save(commit=False)
             post.user=request.user
             post.save()
+            self.pFlag=True
+            model = settings.MODEL
+            tkz=settings.TOKENIZER
             text=form.cleaned_data['post']
             form=HomeForm()
-            return  redirect ('home:home')
+            tagged = tag(model,tkz(post))
+            args = {'form': form, 'posts': post, 'tagged': tagged}
+            return render(request,self.template_name,args)
 
         args={'form':form,'text':text}
         return render(request,self.template_name,args)
-
